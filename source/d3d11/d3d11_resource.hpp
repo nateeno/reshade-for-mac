14e10a946004d70d28f9ff52db2cac14530ae1ce
nathanielbk <115590362+nathanielbk@users.noreply.github.com>
2024-04-02 22:26:50 +0200

    Removed DirectX Vulkan OpenXR

--- source/d3d11/d3d11_resource.hpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2023 Patrick Mours
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#pragma once
-
-void STDMETHODCALLTYPE ID3D11Resource_GetDevice(ID3D11Resource *pResource, ID3D11Device **ppDevice);
